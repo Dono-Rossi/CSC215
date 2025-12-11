405ba9bbe5b33cb33400455fbb75529986efd9b8
Donnie <donovan_rossi@live.com>
2025-12-11 09:36:26 -0500

    yum yum

--- CPM_Altair8800_WinMacLin/backward.C
+++ CPM_Altair8800_WinMacLin/backward.C
@@ -1,33 +1,55 @@
 #include <stdio.h>
 
+#define MAXLINE 1024
+
+reverse(s)
+char s[];
+{
+    int i, j, len;
+    char temp;
+    
+    /* Find length of string */
+    len = 0;
+    while (s[len] != '\0')
+        len++;
+    
+    /* Remove trailing newline if present */
+    if (len > 0 && s[len - 1] == '\n') {
+        s[len - 1] = '\0';
+        len--;
+    }
+    
+    /* Reverse the string in place */
+    for (i = 0, j = len - 1; i < j; i++, j--) {
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
+
 main(argc, argv)
 char *argv[];
 {
     FILE *fp;
-    char linbuf[128];
-    char line;
-    int charnum;
-    int i;
+    char linbuf[MAXLINE];
+    
     if (argc != 2) {
-        printf("Usage: pnum filename <cr>\n");
+        printf("Usage: backward filename <cr>\n");
         exit();
     }
+    
     if ((fp = fopen(argv[1], "r")) == NULL) {
-        printf("Can’t open %s\n", argv[1]);
+        printf("Can't open %s\n", argv[1]);
         exit();
     }
-    charnum = 0;
-    while (fgets(linbuf, 128, fp)){
-        for (i = 0; linbuf[i] != '\0' && linbuf[i] != '\n'; i++) {
-            charnum++;
-        }
-        for (i = 0; i < (charnum/2); i++) {
-            line = linbuf[i];
-            linbuf[i] = linbuf[charnum - i - 1];
-            linbuf[charnum - i - 1] = line;
-        }
-        printf("%s", linbuf);
-        charnum = 0;
+    
+    while (fgets(linbuf, MAXLINE, fp)) {
+        reverse(linbuf);
+        printf("%s\n", linbuf);
     }
+    
     fclose(fp);
 }
+
+
+
