2564dc1a77be07afa52e39a0a638ded1adfdd8c7
Donnie <donovan_rossi@live.com>
2026-01-09 19:45:33 -0500

    d

--- CPM_Altair8800_WinMacLin/biginttd.C
+++ CPM_Altair8800_WinMacLin/biginttd.C
@@ -18,7 +18,51 @@ main() {
         ASSERT_STR(get_bigint(bi1), "2");
         ASSERT_STR(get_bigint(bi2), "4");
         add_bigint(bi1,bi2,biout);
-        ASSERT_STR(get_bigint(biout), "3");
+        ASSERT_STR(get_bigint(biout), "6");
+}
+    TEST_CASE("Add two double digit numbers"){
+        struct bigint bi1;
+        struct bigint bi2;
+        struct bigint biout;
+        set_bigint("12", &bi1);
+        set_bigint("14", &bi2);
+        ASSERT_STR(get_bigint(bi1), "12");
+        ASSERT_STR(get_bigint(bi2), "14");
+        add_bigint(bi1,bi2,biout);
+        ASSERT_STR(get_bigint(biout), "26");
+}
+    TEST_CASE("Add two ten digit numbers"){
+        struct bigint bi1;
+        struct bigint bi2;
+        struct bigint biout;
+        set_bigint("1234567891", &bi1);
+        set_bigint("1987654321", &bi2);
+        ASSERT_STR(get_bigint(bi1), "1234567891");
+        ASSERT_STR(get_bigint(bi2), "1987654321");
+        add_bigint(bi1,bi2,biout);
+        ASSERT_STR(get_bigint(biout), "3222222212");
+}
+    TEST_CASE("Add two different length numbers"){
+        struct bigint bi1;
+        struct bigint bi2;
+        struct bigint biout;
+        set_bigint("2298", &bi1);
+        set_bigint("43", &bi2);
+        ASSERT_STR(get_bigint(bi1), "2298");
+        ASSERT_STR(get_bigint(bi2), "43");
+        add_bigint(bi1,bi2,biout);
+        ASSERT_STR(get_bigint(biout), "2341");
+}
+    TEST_CASE("Add two different length numbers"){
+        struct bigint bi1;
+        struct bigint bi2;
+        struct bigint biout;
+        set_bigint("67", &bi1);
+        set_bigint("6700", &bi2);
+        ASSERT_STR(get_bigint(bi1), "67");
+        ASSERT_STR(get_bigint(bi2), "6700");
+        add_bigint(bi1,bi2,biout);
+        ASSERT_STR(get_bigint(biout), "6767");
 }
     END_TESTING();
 }
